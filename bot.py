4d187be87d03f6fb34044f9a48f5644c36fd196b
CubeMsater <zangiabadymohammad68@gmail.com>
2025-05-29 12:10:40 +0330

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,10 +4,28 @@ from discord.ext import commands
 import aiosqlite
 from config import TOKEN
 from rank_card import generate_rank_card
+from flask import Flask
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
+
+
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+
+
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
